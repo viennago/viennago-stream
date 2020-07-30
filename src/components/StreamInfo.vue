9be3910ef4c5ef312d7784809613e172a0509c4b
Robert Axelsen <robert.axelsen@ebcont.com>
2020-07-30 16:22:00 +0200

    change YT link to direct to new stream

--- src/components/StreamInfo.vue
+++ src/components/StreamInfo.vue
@@ -5,7 +5,7 @@
       Streaming link will be made available here shortly before meetup start:
     </p>
     <div v-if="live">
-      <a href="https://www.youtube.com/watch?v=xLnZbyeEJ-g" target="_blank" rel="noopener noreferrer">YouTube Live Stream</a>
+      <a href="https://www.youtube.com/watch?v=xCYYzPQJPH8" target="_blank" rel="noopener noreferrer">YouTube Live Stream</a>
       <br />
       <a href="https://www.twitch.tv/robchew" target="_blank" rel="noopener noreferrer">Twitch Stream</a>
     </div>
